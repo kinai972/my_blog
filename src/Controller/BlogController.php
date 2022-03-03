be2e9ecbccde0cd2e17b59a5684d116667fb4b7f
Kenny <kennymonique972@gmail.com>
2022-03-03 15:06:10 +0100

    Route article-id + template read

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +20,16 @@ class BlogController extends AbstractController
         $posts = $repository->findAll();
         return $this->render("index.html.twig", [
             "posts" => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/article-{id}", name="blog_read")
+     */
+    public function read(Post $post): Response
+    {
+        return $this->render("read.html.twig", [
+            "post" => $post,
         ]);
     }
 }
