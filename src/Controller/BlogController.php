61d16c20cd0d3b1cb61c3723b8555065289ab1e6
Kenny <kennymonique972@gmail.com>
2022-03-03 17:10:14 +0100

    adding a comment form for articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
@@ -26,10 +30,22 @@ class BlogController extends AbstractController
     /**
      * @Route("/article-{id}", name="blog_read")
      */
-    public function read(Post $post): Response
+    public function read(Post $post, Request $request, EntityManagerInterface $manager): Response
     {
+        $comment = new Comment();
+        $comment->setPost($post);
+        $form = $this->createForm(CommentType::class, $comment)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute("blog_read", ["id" => $post->getId()]);
+        }
+
         return $this->render("read.html.twig", [
             "post" => $post,
+            "form" => $form->createView(),
         ]);
     }
 }
